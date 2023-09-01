69811ad8c66a2df866999e9091a74baf9600eb86
AlkenD <desteralken@gmail.com>
2023-09-01 19:05:10 +0530

    fix: project setup

---
+++ apps/astro-example/src/examples/AwePlayerSvelte.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { AwePlayer } from '@awe-player/svelte'
+    import '@awe-player/tailwind/player.css'
+</script>
+
+<div>
+    <AwePlayer/>
+</div>
---
+++ packages/svelte/src/lib/index.js
@@ -0,0 +1,1 @@
+export { default as AwePlayer } from './AwePlayer.svelte';
