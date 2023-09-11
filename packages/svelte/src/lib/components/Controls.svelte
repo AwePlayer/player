8f755c9322706e25cf09a20d3a86782bdc4ce24e
AlkenD <desteralken@gmail.com>
2023-09-11 13:25:20 +0530

    feat: add 10s forward and backward buttons.

--- packages/svelte/src/lib/components/Controls.svelte
+++ packages/svelte/src/lib/components/Controls.svelte
@@ -28,6 +28,14 @@
 		is_fullscreen = !is_fullscreen;
 		screenfull.toggle(player_instance);
 	}
+
+	function handleForward10() {
+		current_time += 10;
+	}
+
+	function handleBackward10() {
+		current_time -= 10;
+	}
 </script>
 
 <div class="absolute bottom-0 left-0 right-0 top-0 bg-black/40 text-white">
@@ -39,9 +47,9 @@
 		class="absolute bottom-4 left-0 right-0 mx-auto flex w-[calc(100%-32px)] max-w-2xl justify-between space-x-2 rounded-lg bg-black/60 p-2 backdrop-blur-lg"
 	>
 		<div class="flex items-center justify-center space-x-2">
-			<!-- <IconButton icon="skip-previous" /> -->
+			<IconButton icon="replay-10" on:click={handleBackward10} />
 			<IconButton icon={paused ? 'play' : 'pause'} on:click={handlePlayPause} />
-			<!-- <IconButton icon="skip-next" /> -->
+			<IconButton icon="forward-10" on:click={handleForward10} />
 		</div>
 		<div class="relative my-auto h-2.5 w-full">
 			<RangeSlider
