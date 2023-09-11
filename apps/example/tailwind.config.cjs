21de0299fe70e051bf028adeb99560a5351b2299
AlkenD <desteralken@gmail.com>
2023-09-11 18:12:42 +0530

    fix(example): update package.json and tw config

--- apps/example/tailwind.config.cjs
+++ apps/example/tailwind.config.cjs
@@ -1,1 +1,9 @@
-module.exports = require('@awe-player/tailwind/tailwind.config');
+module.exports = {
+	content: [
+		'./src/**/*.{html,js,svelte,ts,astro,jsx,tsx}',
+		'./node_modules/@awe-player/svelte/dist/**/*.{html,js,svelte,ts}',
+	],
+	theme: {
+		extend: {},
+	},
+};
